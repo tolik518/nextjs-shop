139a6e9e9f5e0e8ab6b22027de2ea74d4595084f
Anatolij Vasilev <Anatolij.Vasilev@flyeralarm.com>
2023-04-10 17:25:50 +0200

    configured tailwind

---
+++ components/Title.tsx
@@ -0,0 +1,11 @@
+import { PropsWithChildren } from 'react';
+
+const Title: React.FC<PropsWithChildren>  = ({children}: any) => {
+    return (
+        <h1 className="text-3xl font-bold text-gray-800 dark:text-gray-100">
+            {children}
+        </h1>
+    );
+}
+
+export default Title;
