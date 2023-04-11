5f9bcc3e76c75d286cff6bdd471b0a6c24973368
Anatolij Vasilev <Anatolij.Vasilev@flyeralarm.com>
2023-04-11 19:51:40 +0200

    Formatting, added dark background

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
-import { AppProps } from 'next/app'
-import '../styles/globals.css'
-import React from 'react';
-
+import { AppProps } from "next/app";
+import "../styles/globals.css";
+import React from "react";
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
-}
+  return (
+    <>
+      <div className=" dark:bg-slate-900 min-h-screen">
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
+};
 
 export default App;
