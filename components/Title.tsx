3f45d1871463c78aa81ca7f8e54c4b3d4ff570a1
Anatolij Vasilev <Anatolij.Vasilev@flyeralarm.com>
2023-04-15 19:19:18 +0200

    fixed style

--- components/Title.tsx
+++ components/Title.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren, ReactNode } from "react";
 
 const Title: React.FC<PropsWithChildren> = ({ children }: any) => {
   return (
-    <h1 className="text-3xl font-bold text-gray-800 dark:text-gray-100">
+    <h1 className="font-bold text-gray-800 inline dark:bg-gradient-to-r dark:from-indigo-200 dark:via-sky-400 dark:to-indigo-200 bg-clip-text font-display text-5xl tracking-tight dark:text-transparent">
       {children}
     </h1>
   );
