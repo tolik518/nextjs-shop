a729dfa6b05b665539bcbbf0c78de9e59254a982
Anatolij Vasilev <Anatolij.Vasilev@flyeralarm.com>
2023-04-21 21:43:10 +0200

    added ApiError-class

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,19 @@
+export class ApiError extends Error {
+    constructor(url: string, status: number, time: any) {
+        super(`${time}: Error fetching ${url}: ${status}`);
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, ApiError);
+        }
+        this.name = "ApiError";
+        this.status = status;
+    }
+}
+
 export async function fetchFromApi(url: string) {
     const response = await fetch(url);
-    if (response.status === 404) {
-        throw new Error("Not Found");
+    if (!response.ok) {
+        let time = new Date().toJSON();
+        throw new ApiError(url, response.status, time);
     }
     const json = await response.json();
     return json;
