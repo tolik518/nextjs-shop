4c2d2f22923aa6410442005f63cb127b663ccb11
Anatolij Vasilev <Anatolij.Vasilev@flyeralarm.com>
2023-04-21 21:33:16 +0200

    removed unused products api

--- pages/api/products/index.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import * as prd from '../../../lib/products';
-
-async function handler(req: NextApiRequest, res: NextApiResponse){
-  const products = await prd.getProductsFromApi();
-  res.status(200).json(products)
-}
-
-export default handler;
